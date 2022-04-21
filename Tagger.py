288a1c298db6bebe7d1099be75405688ba796f99
Teamabasof <104113926+Teamabasof@users.noreply.github.com>
2022-04-22 00:40:20 +0400

    update Tagger.py

--- Tagger.py
+++ Tagger.py
@@ -28,9 +28,9 @@ async def start(event):
                     buttons=(
                    
 		      [Button.url('Beni Gruba Ekle ➕', 'https://t.me/lucitaggerbot?startgroup=a')],
-                      [Button.url('Support🛠', 'https://t.me/LuciSup')],
-                      [Button.url('Resmi Kanal📣', 'https://t.me/LuciBots')],
-		      [Button.url('Developer👨🏻‍💻', 'https://t.me/LuciMarka')],
+                      [Button.url('Support🛠', 'https://t.me/teamtagsup')],
+                      [Button.url('Resmi Kanal📣', 'https://t.me/texnoteamm')],
+		      [Button.url('Sahib👨🏻‍💻', 'https://t.me/TTteamabasof')],
                     ),
                     link_preview=False
                    )
@@ -40,9 +40,9 @@ async def help(event):
   await event.reply(helptext,
                     buttons=(
                       [Button.url('Beni Gruba Ekle➕', 'https://t.me/lucitaggerbot?startgroup=a')],
-                      [Button.url('Support👨‍💻', 'https://t.me/LuciSup')],
-                      [Button.url('Resmi Kanal🔖', 'https://t.me/LuciBots')],
-		      [Button.url('Developer🧑‍🔧', 'https://t.me/LuciMarka')],
+                      [Button.url('Support👨‍💻', 'https://t.me/teamtagsup')],
+                      [Button.url('Resmi Kanal🔖', 'https://t.me/texnoteamm')],
+		      [Button.url('Sahib🧑‍🔧', 'https://t.me/TTteamabasof')],
                     ),
                     link_preview=False
                    )
@@ -284,5 +284,5 @@ async def mentionall(tagadmin):
 		sleep(0.5)
 
 
-print(">> Bot çalıyor merak etme 🚀 @lucimarka bilgi alabilirsin <<")
+print(">> Bot çalıyor merak etme 🚀 @TTteamabasof bilgi alabilirsin <<")
 client.run_until_disconnected()
